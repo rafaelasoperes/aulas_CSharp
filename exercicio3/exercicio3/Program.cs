1cb8b1057245d1f610e2ad3ffff22937822648b6
Rafaela <rafaelasoperes@gmail.com>
2023-01-12 15:31:23 -0300

    aulas de matrizes e vetores

---
+++ exercicio3/exercicio3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            N = int.Parse(Console.ReadLine());
+            string[] nomes = new string[N];
+            int[] idades = new int[N];
+            double[] alturas = new double[N];
+
+            // Leitura dos dados
+            for (int i = 0; i < N; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                nomes[i] = s[0];
+                idades[i] = int.Parse(s[1]);
+                alturas[i] = double.Parse(s[2], CultureInfo.InvariantCulture);
+            }
+
+            //Calculo da altura media das pessoas
+            double soma = 0.0;
+            for (int i=0; i< N; i++)
+            {
+                soma= soma + alturas[i];
+            }
+
+            double media = soma / N;
+            Console.WriteLine("Altura média: " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+            // Porcentagem de pessoas abaico de 16 anos
+
+            int cont = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (idades[i] < 16)
+                {
+                    cont++;
+                }
+            }
+            double porcentagem = (double)cont / N * 100.0;
+            Console.WriteLine("Pessoas com menos de 16 anos : " + porcentagem.ToString("F1", CultureInfo.InvariantCulture) + "%");
+         
+        }
+    }
+}
